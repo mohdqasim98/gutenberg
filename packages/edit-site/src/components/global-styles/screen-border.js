243109a0de39a99c8392cd88243e8c19a54f35c8
madhusudhand <madhusudhan.dollu@gmail.com>
2022-12-12 17:43:38 +0530

    add shadow ui controls in global styles
    hide/show default shadows based on the setting from theme.json
    
    add block style variations prefix
    
    rename border menu to border & shadow

--- packages/edit-site/src/components/global-styles/screen-border.js
+++ packages/edit-site/src/components/global-styles/screen-border.js
@@ -10,16 +10,21 @@ import ScreenHeader from './header';
 import BorderPanel, { useHasBorderPanel } from './border-panel';
 import BlockPreviewPanel from './block-preview-panel';
 import { getVariationClassNameFromPath } from './utils';
+import ShadowPanel, { useHasShadowControl } from './shadow-panel';
 
 function ScreenBorder( { name, variationPath = '' } ) {
 	const hasBorderPanel = useHasBorderPanel( name );
 	const variationClassName = getVariationClassNameFromPath( variationPath );
+	const hasShadowPanel = useHasShadowControl( name );
 	return (
 		<>
-			<ScreenHeader title={ __( 'Border' ) } />
+			<ScreenHeader title={ __( 'Border & Shadow' ) } />
 			<BlockPreviewPanel name={ name } variation={ variationClassName } />
 			{ hasBorderPanel && (
 				<BorderPanel name={ name } variationPath={ variationPath } />
+			) }
+			{ hasShadowPanel && (
+				<ShadowPanel name={ name } variationPath={ variationPath } />
 			) }
 		</>
 	);
