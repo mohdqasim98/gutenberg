c2fb75bcc439ec5891eea76a236c67dcd77644ed
apeatling <apeatling@gmail.com>
2023-01-19 12:07:03 -0800

    Update snackbar position in side editor when list view or inserter are open.

--- packages/edit-site/src/components/editor/index.js
+++ packages/edit-site/src/components/editor/index.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import { useMemo } from '@wordpress/element';
@@ -153,6 +158,12 @@ export default function Editor() {
 		return <CanvasSpinner />;
 	}
 
+	const className = classnames( {
+		'show-icon-labels': showIconLabels,
+		'inserter-open': isInserterOpen,
+		'list-view-open': isListViewOpen,
+	} );
+
 	return (
 		<>
 			{ isEditMode && <WelcomeGuide /> }
@@ -168,9 +179,7 @@ export default function Editor() {
 							<SidebarComplementaryAreaFills />
 							<InterfaceSkeleton
 								enableRegionNavigation={ false }
-								className={
-									showIconLabels && 'show-icon-labels'
-								}
+								className={ className }
 								notices={ isEditMode && <EditorSnackbars /> }
 								content={
 									<>
